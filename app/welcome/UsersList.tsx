c54dcf57a015237fe65387f5671a32ebfbb90f6a
Alaina Cook <alaina.leeann35@gmail.com>
2026-09-06 14:54:19 -0400

    project setup

---
+++ app/welcome/UsersList.tsx
@@ -0,0 +1,12 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function UsersList() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        
+      </div>
+    </main>
+  );
+}
